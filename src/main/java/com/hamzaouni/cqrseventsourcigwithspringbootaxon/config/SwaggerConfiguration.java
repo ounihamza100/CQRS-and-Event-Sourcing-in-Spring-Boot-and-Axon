252acfbff27c5ca8d78de343a3a5621e043424a5
ounihamza100 <hamza.ellouni@gmail.com>
2019-12-30 10:17:28 +0100

    polish

--- src/main/java/com/hamzaouni/cqrseventsourcigwithspringbootaxon/config/SwaggerConfiguration.java
+++ src/main/java/com/hamzaouni/cqrseventsourcigwithspringbootaxon/config/SwaggerConfiguration.java
@@ -24,7 +24,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors
-                        .basePackage("com.targa.labs.dev.cqrses"))
+                        .basePackage("com.hamzaouni.cqrseventsourcigwithspringbootaxon"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getApiInfo());

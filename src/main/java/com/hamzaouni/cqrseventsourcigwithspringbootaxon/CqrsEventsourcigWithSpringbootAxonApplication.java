c10e4bff0dae0c9a316fdcdbb83f1625bb3ff6ec
ounihamza100 <hamza.ellouni@gmail.com>
2019-12-30 09:04:13 +0100

    first commit

---
+++ src/main/java/com/hamzaouni/cqrseventsourcigwithspringbootaxon/CqrsEventsourcigWithSpringbootAxonApplication.java
@@ -0,0 +1,13 @@
+package com.hamzaouni.cqrseventsourcigwithspringbootaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsEventsourcigWithSpringbootAxonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsEventsourcigWithSpringbootAxonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamzaouni/cqrseventsourcigwithspringbootaxon/CqrsEventsourcigWithSpringbootAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzaouni.cqrseventsourcigwithspringbootaxon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsEventsourcigWithSpringbootAxonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a190ad4641e3fdf2c59ab1463105cedd779c5770
ounihamza100 <hamza.ellouni@gmail.com>
2019-12-30 11:56:54 +0100

    polish

--- src/main/java/com/hamzaouni/cqrseventsourcigwithspringbootaxon/rest/dto/AccountCreationDTO.java
+++ src/main/java/com/hamzaouni/cqrseventsourcigwithspringbootaxon/rest/dto/AccountCreationDTO.java
@@ -1,15 +1,32 @@
 package com.hamzaouni.cqrseventsourcigwithspringbootaxon.rest.dto;
 
-import lombok.Value;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 /**
  * @author Hamza Ouni
  */
-@Value
+
+
 public class AccountCreationDTO {
-    private final BigDecimal initialBalance;
-    private final String owner;
+    private  BigDecimal initialBalance;
+    private  String owner;
+
+    public AccountCreationDTO() {
+    }
+
+    public AccountCreationDTO(BigDecimal initialBalance, String owner) {
+        this.initialBalance = initialBalance;
+        this.owner = owner;
+    }
+
+    public BigDecimal getInitialBalance() {
+        return initialBalance;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
 }
 
